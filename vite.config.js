0676322a6bda00b975b67bcdf3718e5ead3df7b4
arpitjhajharia <arpitjhajharia@gmail.com>
2026-01-06 22:42:00 +0530

    Update vite.config.js
    Undo changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/arpitjhajharia/LEDTool9/', // Add this line - use your exact GitHub repo name here
 })
